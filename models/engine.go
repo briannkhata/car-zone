f4bbe26d3dd9fd57c9131fb6d99a7a9892f24668
Brian Nkhata <bnkhata@mra.mw>
2024-11-03 11:50:23 +0200

    added validation for models

--- models/engine.go
+++ models/engine.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 
@@ -9,4 +11,25 @@ type Engine struct {
 	Displacement  int64     `json:"displacement"`
 	NoOfCylinders int64     `json:"noOfCylinders"`
 	CarRange      int64     `json:"carRange"`
+}
+
+func validateDisplacement(displacement int64) error {
+	if displacement <= 0 {
+		return errors.New("displacement must be greater than zero")
+	}
+	return nil
+}
+
+func validateNoOfCylinders(noOfCylinders int64) error {
+	if noOfCylinders <= 0 {
+		return errors.New("NoOfCylinders must be greater than zero")
+	}
+	return nil
+}
+
+func validateCarRange(carRange int64) error {
+	if carRange <= 0 {
+		return errors.New("carRange must be greater than zero")
+	}
+	return nil
 }
